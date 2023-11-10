d7d768d580c440aafc0db6f5da056981b34b5c10
Code-with-Abhay <abhayayare007@gmail.com>
2023-11-11 00:50:22 +0530

    number change

--- 14-Day14-If-Else-Conditionals/02 if-else.py
+++ 14-Day14-If-Else-Conditionals/02 if-else.py
@@ -1,6 +1,8 @@
 applePrice = 10
-budget = 200
+budget = 20
 if (budget - applePrice > 50):
     print("Alexa, add 1 kg Apples to the cart.")
+    print("hello")
 else:
     print("Alexa, do not add Apples to the cart.")
+    
